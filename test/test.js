6ba6cef23c36e6cd93d17a5e22071addf497f1a6
Ricky Reusser <rsreusser@gmail.com>
2015-07-31 09:11:45 -0400

    Test local truncation error

--- test/test.js
+++ test/test.js
@@ -52,14 +52,29 @@ Object.keys(ctors).forEach(function(dtype) {
     })
 
     describe('convergence', function() {
+
+      it('location truncation error is order O(h^5)', function() {
+        var result = richardson(function(h) {
+
+          // Integrate long a sector of a circle:
+          var f = function(dydt, y) { dydt[0] = -y[1]; dydt[1] =  y[0] }
+          var i = rk4( new ctor([1,0]), f, 0, h ).step()
+
+          // Return the distance from the expected endpoint:
+          return Math.sqrt( Math.pow(i.y[0]-Math.cos(h),2) + Math.pow(i.y[1]-Math.sin(h),2) )
+
+        }, 2*Math.PI/20, { f: 0 } )
+
+        assert.closeTo( result.n, 5, 1e-2, 'total accumulated error is O(h^5)' )
+      })
+
       it('total accumulated error is order O(h^4)', function() {
 
         var result = richardson(function(h) {
 
           // Integrate around a circle with this step size:
           var f = function(dydt, y) { dydt[0] = -y[1]; dydt[1] =  y[0] }
-          var i = rk4( new ctor([1,0]), f, 0, h )
-          i.steps( Math.floor(2*Math.PI/h + 0.5) )
+          var i = rk4( new ctor([1,0]), f, 0, h ).steps( Math.floor(2*Math.PI/h + 0.5) )
 
           // Return the distance from the expected endpoint:
           return Math.sqrt( Math.pow(i.y[0]-1,2) + Math.pow(i.y[1],2) )
@@ -68,6 +83,7 @@ Object.keys(ctors).forEach(function(dtype) {
 
         assert.closeTo( result.n, 4, 1e-2, 'total accumulated error is O(h^4)' )
       })
+
     })
 
   })
